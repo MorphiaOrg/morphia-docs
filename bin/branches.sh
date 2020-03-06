22cf5ed405ef3953a5f4e342d89774c666191601
evanchooly <jlee@antwerkz.com>
2020-03-06 13:00:00 -0500

    clean publishing now

--- bin/branches.sh
+++ bin/branches.sh
@@ -5,7 +5,6 @@ for i in ../reference/*
 do
   if [ -d $i/content ]
   then
-    echo Registering $i for the landing page
     make -C $i -s version.toml
     cat $i/version.toml >> data/releases.toml
     echo >> data/releases.toml
