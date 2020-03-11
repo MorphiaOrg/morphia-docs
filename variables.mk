864d1468dc7af51c64ba0a1d3a65986bf50d1eb9
evanchooly <jlee@antwerkz.com>
2020-03-10 21:27:53 -0400

    add some filtering

--- variables.mk
+++ variables.mk
@@ -11,10 +11,11 @@ POM = $(MORPHIA_REPO)/pom.xml
 MAVEN_HELP = org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate
 SRC_LINK = "https://github.com/MorphiaOrg/morphia/tree/$(BRANCH)"
 
+FILTER='grep -v "^\[" | grep -v "Download" '
 MAJOR = $(shell echo ${CURRENT} | sed -e 's/-SNAPSHOT//')
-CURRENT = $(shell mvn -f $(POM) $(MAVEN_HELP) -Dexpression=project.version | grep -v INFO | sed -e 's/^r//' )
-ARTIFACT = $(shell mvn -f $(CORE)/pom.xml $(MAVEN_HELP) -Dexpression=project.artifactId | grep -v INFO | sed -e 's/^r//' )
-DRIVER = $(shell mvn -f $(POM) $(MAVEN_HELP) -Dexpression=driver.version | grep -v INFO)
+CURRENT = $(shell mvn -f $(POM) $(MAVEN_HELP) -Dexpression=project.version | grep -v "^\[" | grep -v "Download" )
+ARTIFACT = $(shell mvn -f $(CORE)/pom.xml $(MAVEN_HELP) -Dexpression=project.artifactId | grep -v "^\[" | grep -v "Download" )
+DRIVER = $(shell mvn -f $(POM) $(MAVEN_HELP) -Dexpression=driver.version | grep -v "^\[" | grep -v "Download" )
 RELEASE_STATUS = $(shell [ "`echo $(BRANCH) | grep '^r'`" ] && echo "current" || echo "development" )
 
 COMMON_FILES=$(shell find $(MAKE_ROOT)/reference/common -type f )
