cd799c0d16a3b6dae345d099be642cff88b4b356
evanchooly <jlee@antwerkz.com>
2020-03-13 01:11:17 -0400

    rename master to 1.5.x

--- bin/branches.sh
+++ bin/branches.sh
@@ -2,7 +2,7 @@
 
 mkdir -p data
 > data/releases.toml
-for i in r1.5.8 r1.4.1 r2.0.0-BETA1 master 2.0-dev
+for i in r1.5.8 r1.4.1 r2.0.0-BETA1 1.5.x master
 do
   VERSION=../reference/$i
   if [ -d ${VERSION}/content ]
